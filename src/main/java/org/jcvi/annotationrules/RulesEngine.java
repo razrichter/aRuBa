c05d111006362ace541774c3dc2f2fc6e9e8c1b9
naxelrod <naxelrod@061605af-e407-0410-a945-9c8a22772c06>
2009-06-10 18:01:27 +0000

    Adding RulesEngine and RulesEngineException classes.
    git-svn-id: http://svn.jcvi.org/ANNOTATION/RulesBasedAnnotation/trunk@26731 061605af-e407-0410-a945-9c8a22772c06

---
+++ src/main/java/org/jcvi/annotationrules/RulesEngine.java
@@ -0,0 +1,99 @@
+package org.jcvi.annotationrules;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import org.drools.KnowledgeBase;
+import org.drools.KnowledgeBaseFactory;
+import org.drools.builder.KnowledgeBuilder;
+import org.drools.builder.KnowledgeBuilderError;
+import org.drools.builder.KnowledgeBuilderErrors;
+import org.drools.builder.KnowledgeBuilderFactory;
+import org.drools.builder.ResourceType;
+import org.drools.event.rule.DebugWorkingMemoryEventListener;
+import org.drools.io.ResourceFactory;
+import org.drools.logger.KnowledgeRuntimeLogger;
+import org.drools.logger.KnowledgeRuntimeLoggerFactory;
+import org.drools.runtime.StatefulKnowledgeSession;
+
+public class RulesEngine {
+
+	private KnowledgeBase kbase;
+	private KnowledgeBuilder kbuilder;
+	private StatefulKnowledgeSession ksession;
+	private String logFilename;
+	private boolean debug = false;
+	private KnowledgeRuntimeLogger logger;
+
+	public RulesEngine() {
+		kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
+		kbase = KnowledgeBaseFactory.newKnowledgeBase();
+	}
+
+	public RulesEngine(String logFilename) {
+		this();
+		this.logFilename = logFilename;
+	}
+
+	public RulesEngine(String logFilename, boolean debug) {
+		this();
+		this.debug = debug;
+		this.logFilename = logFilename;
+	}
+
+	public boolean addResource(String rfile, ResourceType rtype) {
+		if (!kbuilder.hasErrors()) {
+			kbuilder.add(ResourceFactory.newClassPathResource(rfile), rtype);
+			KnowledgeBuilderErrors errors = kbuilder.getErrors();
+			if (errors.size() > 0) {
+				for (KnowledgeBuilderError error : errors) {
+					System.err.println(error);
+				}
+				throw new IllegalArgumentException(
+						"Could not parse knowledge in " + rfile);
+			} else {
+				kbase.addKnowledgePackages(kbuilder.getKnowledgePackages());
+				return true;
+			}
+		} else {
+			System.err
+					.println("Attempt to addResource on a KnowledgeBase that already has errors.");
+		}
+		return false;
+	}
+
+	public void addFacts(Iterable<Object> facts) {
+		if (ksession == null)
+			ksession = kbase.newStatefulKnowledgeSession();
+		for (Iterator<Object> iter = facts.iterator(); iter.hasNext();) {
+			Object fact = iter.next();
+			ksession.insert(fact);
+		}
+	}
+
+	public void addGlobals(HashMap<String, Object> global) {
+		if (ksession == null)
+			ksession = kbase.newStatefulKnowledgeSession();
+		for (String globalKey : global.keySet()) {
+			ksession.setGlobal(globalKey, global.get(globalKey));
+		}
+	}
+
+	public void fireAllRules() throws RulesEngineException {
+		if (ksession == null) {
+			throw new RulesEngineException(
+					"Invalid attempt to fire rules without instantiating a knowledge session.");
+		}
+		logger = (logFilename == null) 
+				? KnowledgeRuntimeLoggerFactory.newConsoleLogger(ksession) 
+				: KnowledgeRuntimeLoggerFactory.newFileLogger(ksession, logFilename);
+
+		if (debug) {
+			ksession.addEventListener(new DebugWorkingMemoryEventListener());
+		}
+		ksession.fireAllRules();
+		ksession.dispose();
+		ksession = null;
+		logger.close();
+	}
+
+}
---
+++ src/main/java/org/jcvi/annotationrules/RulesEngineException.java
@@ -0,0 +1,15 @@
+package org.jcvi.annotationrules;
+
+public class RulesEngineException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public RulesEngineException(String message) {
+        super(message);
+    }
+
+    public RulesEngineException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
