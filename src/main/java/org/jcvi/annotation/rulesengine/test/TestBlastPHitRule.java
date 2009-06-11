c29d6467ed14f9473b20b2ddcbbb11982d77ff1e
naxelrod <naxelrod@061605af-e407-0410-a945-9c8a22772c06>
2009-06-11 21:10:34 +0000

    Completed 10 Test Cases for the TestBlastPHitRule, and verified that the tests are successful.
    git-svn-id: http://svn.jcvi.org/ANNOTATION/RulesBasedAnnotation/trunk@26753 061605af-e407-0410-a945-9c8a22772c06

--- src/main/java/org/jcvi/annotation/rulesengine/test/TestBlastPHitRule.java
+++ src/main/java/org/jcvi/annotation/rulesengine/test/TestBlastPHitRule.java
@@ -3,6 +3,7 @@ package org.jcvi.annotation.rulesengine.test;
 
 import java.io.InputStream;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -19,6 +20,7 @@ public class TestBlastPHitRule extends TestCase {
 	private RulesEngine engine;
 	private Feature orf;
 	private Annotation ann;
+	
 	@Before
 	public void setUp() throws Exception {
 		engine = new RulesEngine();
@@ -26,15 +28,13 @@ public class TestBlastPHitRule extends TestCase {
 		engine.addResource(url.toString(), ResourceType.DRL);
 		this.orf = new Feature("testorf", new SourceMolecule(new Genome("bac"), "1"), 0, 110, 1, FeatureType.ORF);
 		
+		// Only hit1 should evaluate true according to our SampleBlastHit rule
 		BlastpHit hit1 = new BlastpHit(orf, "RF|NP_844922.1", 0.001, 170, 170, 0.002,
 				100, 200, 1, 110, 100, 205, 1, 95.0, 82.0);
-		
 		BlastpHit hit2 = new BlastpHit(orf, "RF|NOT_IT.1", 0.001, 170, 170, 0.002,
 				100, 200, 1, 110, 100, 205, 1, 95.0, 82.0);
-		
 		BlastpHit hit3 = new BlastpHit(orf, "RF|NOT_IT.1", 0.001, 170, 170, 0.002,
 				100, 200, 1, 1000, 100, 205, 1, 95.0, 82.0);
-
 		
 		engine.addFact(hit1);
 		engine.addFact(hit2);
@@ -45,26 +45,47 @@ public class TestBlastPHitRule extends TestCase {
 	}
 	
 	public void testSampleBlastCommonName() {
-		assert(this.ann.getCommonName().equals("exosporium protein K"));
+		assertEquals(ann.getCommonName(), "exosporium protein K");
 	}
 	public void testSampleBlastGeneSymbol() {
-		assert(ann.getGeneSymbol().equals("exsK"));
+		assertEquals(ann.getGeneSymbol(), "exsK");
 	}
+	
+	public void testSampleBlastGeneSymbolFails2() {
+		assertFalse(ann.getGeneSymbol() == "not_gene_symbol");
+	}
+
 	public void testSampleBlastEcNumber() {
-		assert(ann.getEcNumber() == null);
+		assertEquals(ann.getEcNumber(), null);
 	}
 	public void testSampleBlastGoIds() {
-		List<String> goIds = ann.getGoIds();
-		assert(goIds.containsAll(Arrays.asList("GO:0043592","GO:0003674","GO:0008150")));
+		ArrayList<String> goIds = new ArrayList<String>();
+		goIds.add("GO:0043592"); 
+		goIds.add("GO:0003674");
+		goIds.add("GO:0008150");
+		assertEquals(goIds, ann.getGoIds());
 	}
 	public void testSampleBlastRoleIds() {
 		List<String> roleIds = ann.getRoleIds();
-		assert(roleIds.contains("705"));
+		assertTrue(roleIds.contains("705"));
 	}
-	public void testFail() {
-		assert(false); // should fail
+	
+	public void testSampleBlastGeneSymbolFails() {
+		assertFalse(ann.getGeneSymbol() == "not_gene_symbol");
+	}
+	
+	public void testSampleBlastSpecificity() {
+		 assertEquals(ann.getSpecificity(), Annotation.EQUIVALOG);
 	}
 
+	public void testSampleBlastAssertionType() {
+		assertEquals(ann.getAssertionType(), Annotation.EXACT);
+	}
+	
+	public void testSampleBlastConfidence() {
+		assertEquals(ann.getConfidence(), 80);
+	}
+	
 	@After
 	public void tearDown() throws Exception {
 		engine = null;
