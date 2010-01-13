cd9e947cf66cbb879b9a83348f82dcf6062c0ef8
naxelrod <naxelrod@061605af-e407-0410-a945-9c8a22772c06>
2010-01-13 16:31:37 +0000

    Added JOC_MGAT_SUBFAM to enumeration of SpecificityType
    git-svn-id: http://svn.jcvi.org/ANNOTATION/RulesBasedAnnotation/trunk@31034 061605af-e407-0410-a945-9c8a22772c06

---
+++ src/org/jcvi/annotation/facts/SpecificityType.java
@@ -0,0 +1,12 @@
+package org.jcvi.annotation.facts;
+
+public enum SpecificityType {
+	ASSERT_GENE,
+	INIT_EQUIV,
+	INITIALIZE,
+	EQUIVALOG,
+	EQUIV_BY_GENE_SYM,
+	OVER_EQUIV,
+	JOC_MGAT_SUBFAM,
+	FAMILY_COMPLETE;
+}
