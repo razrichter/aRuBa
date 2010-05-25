8952891c6228d3d6af07ab837dd37a2d5f93d3e6
naxelrod <naxelrod@061605af-e407-0410-a945-9c8a22772c06>
2010-05-25 17:36:44 +0000

    Added two-step process to genprop.groovy
    git-svn-id: http://svn.jcvi.org/ANNOTATION/RulesBasedAnnotation/trunk@32190 061605af-e407-0410-a945-9c8a22772c06

--- scripts/genprop.groovy
+++ scripts/genprop.groovy
@@ -1,22 +1,28 @@
 #!/usr/bin/env groovy
 
 import org.jcvi.annotation.Aruba
+import org.jcvi.annotation.facts.GenomeProperty
 
+// Create a new instance of our Aruba class
 a = new Aruba()
 a.log()
 
-// a.addGenomeProperties();
+// Load Genome Properties facts and rules
+// a.addGenomeProperties()
 a.addDrools('/org/jcvi/annotation/rules/genomeproperties/suffices.drl')
 a.addDrools('/org/jcvi/annotation/rules/genomeproperties/AboveTrustedCutoff.drl')
-
 a.addGenomePropertiesFacts()
 
+// Load facts from the Small Genome database
 a.addSmallGenome('gb6')
 
+// Run the first phase
 a.run()
 
+// Add rule to compute Genome Property values
 a.addDrools('/org/jcvi/annotation/rules/genomeproperties/requiredby.drl')
 
 a.run()
 
-GenomeProperty.report(System.out);
+// Detailed report
+GenomeProperty.detailReport(System.out);
